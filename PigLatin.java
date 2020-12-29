31e533032c3e7420792e3a045f6ef98c35e30b24
jzhowie <jzhou20@stuy.edu>
2020-12-29 09:32:23 -0500

    Added uppercase handling to both pigLatin methods

--- PigLatin.java
+++ PigLatin.java
@@ -8,45 +8,49 @@ import java.io.*;
 public class PigLatin {
 public static void main(String[] args) {
 	System.out.println("/// PIG LAINT SIMPLE /// (Test cases from Rocket Chat)");
-	System.out.println(pigLatinSimple("mock"));
-	System.out.println(pigLatinSimple("pie"));
-	System.out.println(pigLatinSimple("david"));
-	System.out.println(pigLatinSimple("aaron"));
+	System.out.println(pigLatinSimple("moCk"));
+	System.out.println(pigLatinSimple("pIe"));
+	System.out.println(pigLatinSimple("dAvid"));
+	System.out.println(pigLatinSimple("Aaron"));
 	System.out.println();
 
 	System.out.println("/// PIG LAINT NORMAL /// (Test cases from Rocket Chat)");
-	System.out.println(pigLatin("the"));
-	System.out.println(pigLatin("check"));
-	System.out.println(pigLatin("skee"));
-	System.out.println(pigLatin("emu"));
-	System.out.println(pigLatin("grade"));
+	System.out.println(pigLatin("The"));
+	System.out.println(pigLatin("checK"));
+	System.out.println(pigLatin("Skee"));
+	System.out.println(pigLatin("eMu"));
+	System.out.println(pigLatin("gRade"));
 }
 
 public static String pigLatinSimple(String s) {
+	String temp = s.toLowerCase();
 	ArrayList<Character> vowels = new ArrayList<Character>(Arrays.asList(new Character[] {'a', 'e', 'i', 'o', 'u'}));
 	String pig = "";
-	if (vowels.contains(s.charAt(0))) {
-		pig = s + "hay";
+
+	if (vowels.contains(temp.charAt(0))) {
+		pig = temp + "hay";
 		return pig;
 	}
-	pig = s.substring(1,s.length()) + s.charAt(0) + "ay";
+	pig = temp.substring(1,temp.length()) + temp.charAt(0) + "ay";
 	return pig;
 }
 
 public static String pigLatin(String s) {
+	String temp = s.toLowerCase();
 	ArrayList<Character> vowels = new ArrayList<Character>(Arrays.asList(new Character[] {'a', 'e', 'i', 'o', 'u'}));
 	ArrayList<String> digraphs = new ArrayList<String>(Arrays.asList(new String[] {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"}));
 	String pig = "";
-	if (s.length() >= 2 && digraphs.contains(s.substring(0, 2))) {
-		pig = s.substring(2, s.length()) + s.substring(0, 2) + "ay";
+
+	if (temp.length() >= 2 && digraphs.contains(temp.substring(0, 2))) {
+		pig = temp.substring(2, temp.length()) + temp.substring(0, 2) + "ay";
 		return pig;
 	}
-	else if (vowels.contains(s.charAt(0))) {
-		pig = s + "hay";
+	else if (vowels.contains(temp.charAt(0))) {
+		pig = temp + "hay";
 		return pig;
 	}
 	else {
-		pig = s.substring(1,s.length()) + s.charAt(0) + "ay";
+		pig = temp.substring(1,temp.length()) + temp.charAt(0) + "ay";
 		return pig;
 	}
 }
